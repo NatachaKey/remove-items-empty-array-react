66154ea4d0424655dc33b996b9219c794a881d53
Natacha Key <nataliaklueva@hotmail.com>
2022-11-07 18:46:39 +0100

    list of presents- react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [gifts, setGifts]= useState(data);
+ 
+  const removeGift= (id)=>{
+   
+    let newGifts = gifts.filter(gift=> gift.id !==id)
+    console.log(newGifts)
+    setGifts(newGifts);
+  } 
+
+  return(
+  <div>
+    <div className="container">
+      <h1>List of {gifts.length}</h1>
     </div>
-  );
+
+    {gifts.map((element => {
+      const {id, gifty, image} = element;
+      
+      return ( <div key={id}>
+        <div className="container">
+        <h2> {id} - {gifty}</h2>
+        </div>
+
+        <div className="container">
+          <img src={image} alt="foto" width="300px"/>
+        </div>
+
+        <div className="container">
+        <button onClick= {()=> removeGift (id)}>Remove</button>
+        </div>
+      </div> )
+    }))}
+
+    <div className="container">
+      <button onClick={()=>setGifts([])}>Delete all</button>
+    </div>
+  </div>
+  )
 }
 
 export default App;
+
+
+
+
+
---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const data = [
+
+  {
+
+    id: 1,
+
+    gifty: "Running shoes",
+
+    image: "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHNob2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+
+  },
+
+  {
+
+    id:    2,
+
+    gifty: "Chocolate",
+
+    image: "https://images.unsplash.com/photo-1549007994-cb92caebd54b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNob2NvbGF0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+
+  },
+
+  {
+
+    id: 3,
+
+    gifty: "Coat",
+
+    image: "https://images.unsplash.com/photo-1591900947067-851789555ef3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNvYXR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+
+  }, 
+
+  {
+
+    id: 4,
+
+    gifty: "AirPods",
+
+    image: "https://images.unsplash.com/photo-1588423771073-b8903fbb85b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=683&q=80"
+
+  },
+
+  {
+
+    id: 5,
+
+    gifty: "Dishes",
+
+    image: "https://images.unsplash.com/photo-1603199506016-b9a594b593c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBsYXRlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+
+  },
+
+  {
+
+    id: 6,
+
+    gifty: "Leggings",
+
+    image: "https://images.unsplash.com/photo-1506629082955-511b1aa562c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+
+  }
+
+]
+
+
